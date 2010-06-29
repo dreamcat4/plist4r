26187619e52f596c49b57398ace52718ca58e770
Dreamcat4 <dreamcat4@gmail.com>
2010-06-29 16:50:21 +0100

    Update test sandbox script

--- test.rb
+++ test.rb
@@ -1,38 +1,65 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
 require 'lib/plist4r'
 
-# puts "Backends = #{::Plist4r::Config[:backends].inspect}"
-# Plist4r.new
+def output
+  o = Plist4r::Backend::Test::Output.new
+  puts o
+  o.write_html_file
+end
+output
 
-Plist4r::Config[:backends].replace ["haml","libxml4r"]
-p = Plist4r.open "plists/array_mini.xml"
-
-puts p.inspect
-# puts p.to_hash.inspect
-# puts p.to_xml
-
-# b = p.to_binary
-# puts b.inspect
+def start_irb
+  # cd tmp/plist4r
+  # irb -r 'rubygems' -r 'lib/plist4r'
+end
 
 
-# p2 = b.to_plist
-# puts p2.inspect
-# puts p2.to_xml
 
-# puts p2.to_xml
-# puts "plist type is"
-# puts p2.plist_type.inspect
+def general_test
+  Plist4r.new
 
-# p2.strict_keys false
-# puts p2.strict_keys.inspect
-# p2.<< do
-#   somekey "append"
-# end
+  Plist4r::Config[:raise_any_failure] = true
+  # Plist4r::Config[:backends].replace ["haml","libxml4r","ruby_cocoa"]
+  # Plist4r::Config[:backends].replace ["osx_plist"]
+  Plist4r::Config[:backends].replace ["c_f_property_list"]
+  puts `pwd`
+  Plist4r::Config[:default_path] = "spec/data"
+  # puts Plist4r::Config[:default_path]
+  p = Plist4r.open "manual/array_mini.xml"
+  p = Plist4r.open "manual/example_medium_launchd.xml"
+  # p = Plist4r.open "manual/example_big_binary.plist"
 
-# puts p2.to_hash.inspect
-# puts p2.to_xml
+  # Plist4r.new do
+  # end
 
+  # puts p.inspect
+  puts p.to_hash.inspect
+  puts p.to_xml
+
+  b = p.to_binary
+  puts b.inspect
+
+
+  p2 = b.to_plist
+  # puts p2.inspect
+  puts p2.to_xml
+
+  # puts p2.to_xml
+  # puts "plist type is"
+  # puts p2.plist_type.inspect
+
+  # p2.strict_keys false
+  # puts p2.strict_keys.inspect
+  # p2.<< do
+  #   somekey "append"
+  # end
+
+  # puts p2.to_hash.inspect
+  # puts p2.to_xml
+end
+# general_test
 
 
 
