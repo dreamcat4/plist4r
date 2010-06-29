d66cd9c680692b6494129409954adf2e53639087
Dreamcat4 <dreamcat4@gmail.com>
2010-06-29 16:58:39 +0100

    We dont seem to need valid_keys when they're empty

--- lib/plist4r/plist_type/info.rb
+++ lib/plist4r/plist_type/info.rb
@@ -3,9 +3,6 @@ require 'plist4r/plist_type'
 
 module Plist4r
   class PlistType::Info < PlistType
-    def self.valid_keys
-      {}
-    end
   end
 end
 
--- lib/plist4r/plist_type/plist.rb
+++ lib/plist4r/plist_type/plist.rb
@@ -3,8 +3,5 @@ require 'plist4r/plist_type'
 
 module Plist4r
   class PlistType::Plist < PlistType
-    def self.valid_keys
-      {}
-    end
   end
 end
