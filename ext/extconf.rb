106fa7f99fe5475727c574f588e85fdb9f0c9689
Dreamcat4 <dreamcat4@gmail.com>
2011-10-06 14:28:21 +0100

    Fix for #2 Not installable on Ubuntu (2nd attempt)

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,8 +5,8 @@ require 'mkmf'
 if File.exists? "/System/Library/Frameworks/CoreFoundation.framework"
   $LDFLAGS += ' -framework CoreFoundation -undefined suppress -flat_namespace'
   $LIBRUBYARG_SHARED=""
-  create_makefile("plist4r/backend/osx_plist/ext/osx_plist")
+  create_makefile("plist4r/backend/osx_plist/ext/osx_plist","osx_plist")
 else
-  create_makefile("")
+  create_makefile("","null")
 end
 
